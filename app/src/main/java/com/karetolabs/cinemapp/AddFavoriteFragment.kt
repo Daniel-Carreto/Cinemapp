bed5d6222ce8d86ce7b335c6107966eae96c76f2
Daniel Castillo <daniel.castillo@metafinanciera.com>
2022-04-02 10:47:48 -0600

    Se agregaron validaciones de campo y los spinners

--- app/src/main/java/com/karetolabs/cinemapp/AddFavoriteFragment.kt
+++ app/src/main/java/com/karetolabs/cinemapp/AddFavoriteFragment.kt
@@ -2,15 +2,24 @@ package com.karetolabs.cinemapp
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.DatePicker
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.karetolabs.cinemapp.databinding.FragmentAddFavoriteBinding
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.regex.Pattern
 
 
 class AddFavoriteFragment : Fragment() {
@@ -60,6 +69,115 @@ class AddFavoriteFragment : Fragment() {
             picker.show(childFragmentManager, materialDatePicker.toString())
 
         }
+
+        ///Spinners
+        val genres = listOf<String>("Selecciona el genero","Romantico", "Comedia", "Sci-Fi", "Terror")
+        val adapterGenres = ArrayAdapter<String>(
+            requireActivity(),
+            android.R.layout.simple_spinner_dropdown_item,
+            genres
+        )
+        fragmentAddFavoriteBinding.spinnerGenre.adapter = adapterGenres
+        fragmentAddFavoriteBinding.spinnerGenre.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if(position == 0){
+                    return
+                }
+                showMessage(genres[position])
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                showMessage("Nada seleccionado")
+            }
+
+        }
+        fragmentAddFavoriteBinding.tietGenre.setAdapter(adapterGenres)
+        fragmentAddFavoriteBinding.tietGenre.onItemClickListener = object: AdapterView.OnItemClickListener{
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                showMessage(genres[position])
+            }
+
+        }
+        //Validations
+        fragmentAddFavoriteBinding.tietImage.doOnTextChanged { text, start, before, count ->
+            Log.d("ADDFavoriteFragment","$text=$start=$before=$count")
+            if(!isValidUrl(text.toString())){
+                fragmentAddFavoriteBinding.tietImage.error = "La url no es valida"
+            }
+//            if(android.util.Patterns.WEB_URL.matcher(text.toString()).matches()){
+//                fragmentAddFavoriteBinding.tilImage.error = ""
+//            }else {
+//                fragmentAddFavoriteBinding.tilImage.error = "Url no es valida"
+//            }
+        }
+        fragmentAddFavoriteBinding.tietImage.addTextChangedListener {
+            Log.d("ADDFavoriteFragment","${it.toString()}")
+        }
+
+        fragmentAddFavoriteBinding.tietImage.addTextChangedListener(changeTextListener)
+
+        fragmentAddFavoriteBinding.btnSave.setOnClickListener {
+            validateFields()
+        }
+
+    }
+
+    private fun isValidUrl(url:String):Boolean{
+        val patter = Pattern.compile("(https?:\\/\\/)?([\\w\\d]+\\.)?[\\w\\d]+\\.\\w+\\/?.+")
+        val matcher = patter.matcher(url)
+        return matcher.matches()
+    }
+
+    private fun validateFields() {
+        if (fragmentAddFavoriteBinding.tietTitle.text.toString().isEmpty()) {
+            fragmentAddFavoriteBinding.tietTitle.error = "El campo no debe ir vacio"
+            fragmentAddFavoriteBinding.tietTitle.requestFocus()
+            return
+        }
+        if (fragmentAddFavoriteBinding.tietSummary.text?.length ?: 0 < 30) {
+            fragmentAddFavoriteBinding.tilSummary.error = "La descripcion debe ser mas amplia"
+            fragmentAddFavoriteBinding.tilSummary.requestFocus()
+            return
+        }
+        showMessage("Exitoso")
+    }
+
+
+    private val changeTextListener = object: TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            fragmentAddFavoriteBinding.tietImage.removeTextChangedListener(this)
+            //Agregamos validaciones
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            fragmentAddFavoriteBinding.tietImage.addTextChangedListener(this)
+        }
+
+    }
+
+    private fun showMessage(message:String){
+        Toast.makeText(requireActivity(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+       // fragmentAddFavoriteBinding.tietImage.removeTextChangedListener(changeTextListener)
+       // fragmentAddFavoriteBinding.tietImage.addTextChangedListener(null)
+
     }
 
 }
